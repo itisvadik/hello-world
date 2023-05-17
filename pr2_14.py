3811d81b645d1bf80d18e2932fce6809202f5afe
itisvadik <98023026+itisvadik@users.noreply.github.com>
2023-05-17 13:43:26 +0300

    Delete pr2_14.py

--- pr2_14.py
+++
@@ -1,8 +0,0 @@
-# yAAx_12 + x02y_14
-for x in '123456789AB':
-    for y in '123456789AB':
-        for A in '123456789AB':
-            t = int(f'{y}{A}{A}{x}', 12) + int(f'{x}02{y}', 14)
-            if t % 80 == 0:
-                t = t // 80
-                print(t)
