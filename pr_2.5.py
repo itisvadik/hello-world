0290eac8e84f98b6696f3e59152d978be17463c3
itisvadik <98023026+itisvadik@users.noreply.github.com>
2023-05-17 13:47:48 +0300

    Delete pr_2.5.py

--- pr_2.5.py
+++
@@ -1,7 +0,0 @@
-print('x y z w')
-for x in range(2):
-    for y in range(2):
-        for z in range(2):
-            for w in range(2):
-                if (((not(z == w)) <= (w and not x)) or (x and not y)) == 0:
-                    print(x, y, z, w)
